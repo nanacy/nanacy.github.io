d31a9b0cebb7df494fdeb64ecedf231989af2ef3
nancy <nancy@nancy.VivoBook>
2018-06-27 10:45:24 +0200

    ajout javascript

---
+++ hello.js
@@ -0,0 +1,8 @@
+function popup(){
+	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+	//mettre en rouge
+}
+
+setTimeout(popup, 10000);
+
+//****************************************
